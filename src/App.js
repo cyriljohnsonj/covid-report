c2171b17e0bd19c1894172e40e6d33c756dbad16
cyriljohnsonj <68160872+cyriljohnsonj@users.noreply.github.com>
2020-07-24 00:57:06 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Card,
+  Select,
+  MenuItem,
+  FormControl
+} from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 REPORT</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+            >
+              <MenuItem>Hello</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+      <Card className="app__left">
+      </Card>
     </div>
   );
 }
